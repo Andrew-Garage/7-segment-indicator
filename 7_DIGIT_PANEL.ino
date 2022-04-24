91a914ee372467aa64fe15db6430314f7cddbce9
Andrew-Garage <89803784+Andrew-Garage@users.noreply.github.com>
2022-04-24 16:26:42 +0300

    Add files via upload

---
+++ 7_DIGIT_PANEL.ino
@@ -0,0 +1,184 @@
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define F 7
+#define G 8
+#define DP A2
+#define D1 12
+#define D2 11 
+#define D3 10
+#define D4 9
+char digit[] = {D1,D2,D3,D4};
+char num[] = {A,B,C,D,E,F,G};
+
+void setup() {
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(DP, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);  
+  digitalWrite(D3, HIGH);  
+  digitalWrite(D4, HIGH);
+}
+
+void loop() {
+  digitalWrite(D1, LOW);
+  for(int x = 0; x < sizeof(num); x++){
+  digitalWrite(num[x], HIGH);}
+  
+}
+
+void zero(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+  
+
+void one(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+void two(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);
+  }
+
+void three(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void four(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void five(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void six(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void seven(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void eight(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+  void nine(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+  delay(3);
+  digitalWrite(x, HIGH);}
+
+
+  void test(char x){
+  digitalWrite(x, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, HIGH);}
